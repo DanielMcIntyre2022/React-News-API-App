48144f0d23dc5632a6f16b020f5046fdf6ff365d
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-28 16:40:53 -0500

    successful api response

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`)
+.then(response => {
+  console.log(response)
+});
 
 function App() {
+
+  const [ news, setNews ] = useState();
+
   return (
     <div className="App">
       <h1>News App</h1>
